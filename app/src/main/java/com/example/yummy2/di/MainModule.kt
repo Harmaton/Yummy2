a8817aae0e661d865d55ead188d9215ca281519f
Harmaton <njagiiharmaton@gmail.com>
2022-06-24 16:52:25 +0300

    splash completed

--- app/src/main/java/com/example/yummy2/di/MainModule.kt
+++ app/src/main/java/com/example/yummy2/di/MainModule.kt
@@ -1,4 +1,19 @@
 package com.example.yummy2.di
 
+import android.content.Context
+import com.example.yummy2.data.DataStoreRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object MainModule {
+
+    @Provides
+    @Singleton
+    fun ProvideDataStoreRepository ( @ApplicationContext context: Context ) = DataStoreRepository(context = context)
 }
---
+++ app/src/main/java/com/example/yummy2/viewmodels/WelcomeViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.yummy2.viewmodels
+
+class WelcomeViewModel {
+}
